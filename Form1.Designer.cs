dc788a35fa285d7795fa9d8945e67d41b29d73db
Aswun <aswinlutfian32@gmail.com>
2025-07-04 00:26:43 +0700

    testing form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,32 +23,30 @@
             this.lblPassword = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
-
             this.mainLayout.SuspendLayout();
             this.panelContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             this.SuspendLayout();
-
             // 
             // mainLayout
             // 
+            this.mainLayout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(40)))), ((int)(((byte)(30)))));
             this.mainLayout.ColumnCount = 1;
             this.mainLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.mainLayout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainLayout.BackColor = System.Drawing.Color.FromArgb(20, 40, 30);
             this.mainLayout.Controls.Add(this.panelContainer, 0, 0);
+            this.mainLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainLayout.Location = new System.Drawing.Point(0, 0);
+            this.mainLayout.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.mainLayout.Name = "mainLayout";
             this.mainLayout.RowCount = 1;
             this.mainLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.mainLayout.Size = new System.Drawing.Size(1024, 600);
+            this.mainLayout.Size = new System.Drawing.Size(1152, 750);
             this.mainLayout.TabIndex = 0;
-
             // 
             // panelContainer
             // 
-            this.panelContainer.BackColor = System.Drawing.Color.FromArgb(40, 80, 60);
             this.panelContainer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(80)))), ((int)(((byte)(60)))));
             this.panelContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelContainer.Controls.Add(this.pictureBoxLogo);
             this.panelContainer.Controls.Add(this.lblTitle);
@@ -57,114 +55,110 @@
             this.panelContainer.Controls.Add(this.lblPassword);
             this.panelContainer.Controls.Add(this.txtPassword);
             this.panelContainer.Controls.Add(this.btnLogin);
-            this.panelContainer.Location = new System.Drawing.Point(282, 90);
-            this.panelContainer.Margin = new System.Windows.Forms.Padding(10);
+            this.panelContainer.Location = new System.Drawing.Point(317, 113);
+            this.panelContainer.Margin = new System.Windows.Forms.Padding(11, 12, 11, 12);
             this.panelContainer.Name = "panelContainer";
-            this.panelContainer.Padding = new System.Windows.Forms.Padding(20);
-            this.panelContainer.Size = new System.Drawing.Size(460, 420);
+            this.panelContainer.Padding = new System.Windows.Forms.Padding(22, 25, 22, 25);
+            this.panelContainer.Size = new System.Drawing.Size(517, 524);
             this.panelContainer.TabIndex = 0;
-
             // 
             // pictureBoxLogo
             // 
             this.pictureBoxLogo.Image = global::ITCourseCertificateV001.Properties.Resources.logo_certificate;
-            this.pictureBoxLogo.Location = new System.Drawing.Point(175, 25);
+            this.pictureBoxLogo.Location = new System.Drawing.Point(197, 31);
+            this.pictureBoxLogo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBoxLogo.Name = "pictureBoxLogo";
-            this.pictureBoxLogo.Size = new System.Drawing.Size(100, 75);
+            this.pictureBoxLogo.Size = new System.Drawing.Size(112, 94);
             this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxLogo.TabIndex = 0;
             this.pictureBoxLogo.TabStop = false;
-
             // 
             // lblTitle
             // 
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
             this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(30, 100);
+            this.lblTitle.Location = new System.Drawing.Point(34, 125);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(400, 40);
+            this.lblTitle.Size = new System.Drawing.Size(450, 50);
             this.lblTitle.TabIndex = 1;
-            this.lblTitle.Text = "Selamat Datang - Login";
+            this.lblTitle.Text = "Selamat Tidur - Login";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
             // 
             // lblUsername
             // 
             this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.lblUsername.ForeColor = System.Drawing.Color.White;
-            this.lblUsername.Location = new System.Drawing.Point(50, 160);
+            this.lblUsername.Location = new System.Drawing.Point(56, 200);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(100, 25);
+            this.lblUsername.Size = new System.Drawing.Size(112, 31);
             this.lblUsername.TabIndex = 2;
             this.lblUsername.Text = "Username";
-
             // 
             // txtUsername
             // 
-            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtUsername.Location = new System.Drawing.Point(50, 185);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(350, 30);
-            this.txtUsername.TabIndex = 3;
             this.txtUsername.BackColor = System.Drawing.Color.WhiteSmoke;
-
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtUsername.Location = new System.Drawing.Point(56, 231);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(393, 34);
+            this.txtUsername.TabIndex = 3;
             // 
             // lblPassword
             // 
             this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.lblPassword.ForeColor = System.Drawing.Color.White;
-            this.lblPassword.Location = new System.Drawing.Point(50, 225);
+            this.lblPassword.Location = new System.Drawing.Point(56, 281);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(100, 25);
+            this.lblPassword.Size = new System.Drawing.Size(112, 31);
             this.lblPassword.TabIndex = 4;
             this.lblPassword.Text = "Password";
-
             // 
             // txtPassword
             // 
+            this.txtPassword.BackColor = System.Drawing.Color.WhiteSmoke;
             this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.txtPassword.Location = new System.Drawing.Point(50, 250);
+            this.txtPassword.Location = new System.Drawing.Point(56, 312);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(350, 30);
+            this.txtPassword.Size = new System.Drawing.Size(393, 34);
             this.txtPassword.TabIndex = 5;
             this.txtPassword.UseSystemPasswordChar = true;
-            this.txtPassword.BackColor = System.Drawing.Color.WhiteSmoke;
-
             // 
             // btnLogin
             // 
-            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(0, 150, 100);
-            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(100)))));
             this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(175, 310);
+            this.btnLogin.Location = new System.Drawing.Point(197, 388);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(110, 35);
+            this.btnLogin.Size = new System.Drawing.Size(124, 44);
             this.btnLogin.TabIndex = 6;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
-
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1024, 600);
+            this.ClientSize = new System.Drawing.Size(1152, 750);
             this.Controls.Add(this.mainLayout);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MinimizeBox = false;
             this.Name = "FormLogin";
             this.Text = "Login Certificate Course";
             this.Load += new System.EventHandler(this.FormLogin_Load);
-
             this.mainLayout.ResumeLayout(false);
             this.panelContainer.ResumeLayout(false);
             this.panelContainer.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
             this.ResumeLayout(false);
+
         }
 
         #endregion
